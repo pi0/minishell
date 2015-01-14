1ddc190477a75aad852113036383e3d0b9222b36
pi0 <pyapar@gmail.com>
2015-01-15 01:12:24 +0330

    Initial commit

---
+++ Shell.cpp
@@ -0,0 +1,231 @@
+/*
+Source code for Shell Project
+
+You have to implement your functions here
+
+Basic programming course
+Dr.Shiri - Amirkabir university @ 2014-2015
+
+Revision : 1.0
+*/
+
+#include "Shell.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+	
+	struct user* first_user=load_users();
+	
+	struct dir* home_dir = dir("home",1);
+	
+	while(true){
+	
+		struct session* s = do_login(first_user,home_dir);
+		
+		printf("User %s successfully logged in !\n",s->user->username);
+		
+		while(true) {
+			printf("%s$ ",s->user->username);
+			
+			char cmd[100];
+			scanf("%s",cmd);
+			if(strcmp(cmd,"exit")==0)break;
+			
+			int return_code = exec_command(s,cmd,first_user);
+			if(return_code!=0)
+				printf("Error executing command or command not implemented !\n");	
+		}
+		save_users(first_user);
+		printf("Logged out !\n");
+	}
+	
+	return 0;
+}
+
+//-----------------------------------------------------------------------------------
+
+struct user* user(const char* username,const char* password,bool is_root) {
+	struct user* user = (struct user*)malloc(sizeof(struct user));
+
+	strcpy(user->username,username);
+	strcpy(user->password,password);
+	user->is_root=is_root;
+	user->next_user=NULL;
+	
+	return user;
+}
+
+//-----------------------------------------------------------------------------------
+
+struct user* load_users() {
+	struct user* first_user;
+	
+	//TODO : implement me !
+	first_user = user("jack","123",0);
+	
+	return first_user;
+}
+
+//-----------------------------------------------------------------------------------
+
+void save_users(struct user* first_user) {
+
+
+	//TODO : implement me !
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+struct session* session(struct user* user,struct dir* home_dir,struct dir* current_dir) {
+	struct session* s = (struct session*)malloc(sizeof(struct session));
+
+	s->user=user;
+	s->home_dir=home_dir;
+	s->current_dir=current_dir;
+	
+	return s;
+}
+
+//-----------------------------------------------------------------------------------
+
+struct session* do_login(struct user* users,struct dir* home_dir) {
+	
+	
+	//TODO : implement me !
+	return session(&users[0],home_dir,home_dir);
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+struct dir* dir(const char* name,bool is_home) {
+	struct dir* dir = (struct dir*)malloc(sizeof(struct dir));
+	strcpy(dir->name,name);
+	dir->is_home=is_home;
+	dir->childs[0]=NULL;
+	return dir;
+}
+
+//-----------------------------------------------------------------------------------
+
+char** tokenize(const char* command) {
+	
+	
+	//TODO : implement me !
+	return NULL;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+struct dir* getDir(struct session* session,const char* path) {
+	
+	
+	//TODO : implement me !
+	return NULL;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int exec_command(struct session* session,const char* cmd,struct user* users) {
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+
+int pwd(struct session* session) {
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int mkdir(struct session* session,const char* name,const char* path){
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int cd(struct session* session,const char* path){
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int ls(struct session* session,const char* path){
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int addUser(struct session* session,const char* name,struct user* users){
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int rmUser(struct session* session,const char* name){
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+int passwd(struct session* session,struct user* users){
+	
+	
+	//TODO : implement me !
+	return -1;
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+//TODO : implement su command
+//int su(.......) {
+
+
+//}
---
+++ Shell.h
@@ -0,0 +1,115 @@
+/*
+Header file for Shell Project
+
+You can use this template for implementing your project
+
+feel free to do any modifications, you are not forced !
+should have a reasonable reason for changing some parts of it !
+(  )
+
+Also keep in your mind that Global Variables Are Bad !!
+	-> http://c2.com/cgi/wiki?GlobalVariablesAreBad 
+
+	
+Basic programming course
+Dr.Shiri @ 2014-2015
+
+
+Revision : 1.0
+*/
+
+#pragma once //Read more about this : http://www.cprogramming.com/reference/preprocessor/pragma.html
+
+#define max_dir_childs 32
+
+#define bool char // bool exists in the current ANSI C - C99, but not in C89/90 !
+
+//-------------------------------------------------------------------------------------------------
+//--- Structures
+//-------------------------------------------------------------------------------------------------
+
+struct dir {
+	char name[30];
+	bool is_home;
+	struct dir *childs[max_dir_childs];//null terminated
+};
+
+struct user {
+	char username[30];
+	char password[30];
+	bool is_root;
+	
+	//you can use it for implementing a linked list
+	struct user* next_user;
+};
+
+struct session {
+	struct user* user;
+	struct dir* home_dir;
+	struct dir* current_dir;
+};
+
+//-------------------------------------------------------------------------------------------------
+//--- Some helper functions
+//-------------------------------------------------------------------------------------------------
+
+//create new user
+struct user* user(const char* username,const char* password,bool is_root);
+
+//load users file passwd.txt into a Linked list of User
+struct user* load_users();
+
+//Save back users file to passwd.txt
+void save_users(struct user* first_user);
+
+//create new session
+struct session* session(struct user* user,struct dir* home_dir,struct dir* current_dir);
+
+//do login
+struct session* do_login(struct user* users,struct dir* home_dir);
+
+//create new directory structure
+struct dir* dir(const char* name,bool is_home);
+
+//Splits command with space to an array of strings (char*)
+char** tokenize(char* command);
+
+//Returns pointer to directory from path
+struct dir* getDir(struct session* session,const char* path);
+
+//Executes the command
+int exec_command(struct session* session,const char* cmd,struct user* users);
+
+//-------------------------------------------------------------------------------------------------
+// Commands 
+// they return an integer number ( 0 of no error or any other number in case of error )
+//-------------------------------------------------------------------------------------------------
+
+//print current working directory's full address
+int pwd(struct session* session);
+
+//Create directory with name 
+int mkdir(struct session* session,const char* name,const char* path);
+
+//Changes session->current_dir to new path
+//Don't forget to check permissions !
+int cd(struct session* session,const char* path);
+
+//Print contents of current directory or path if path is not NULL
+int ls(struct session* session,const char* path);
+
+//Create a user 
+//Don't forget to check permissions !
+int addUser(struct session* session,const char* name,struct user* users);
+
+//Removes a user
+//Don't forget to check permissions !
+int rmUser(struct session* session,const char* name); // Users or root cannot delete itself !!
+
+//Changes current user password
+//Don't forget to check permissions !
+int passwd(struct session* session,struct user* users);
+
+
+//TODO : define su command
+//int su(.......);
